9ebf4a5f67920cf310bfdc8d8c60093f0353102f
PsorTheDoctor <psor2.0@gmail.com>
2020-08-13 02:24:33 +0200

    Initial commit

---
+++ app/src/main/java/com/wolkowycki/android_chart/MainActivity.java
@@ -0,0 +1,48 @@
+package com.wolkowycki.android_chart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    LineChart lineChart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lineChart = (LineChart) findViewById(R.id.chart);
+        LineDataSet lineDataSet = new LineDataSet(dataValues(), "Bitcoin prices");
+
+        lineDataSet.setLineWidth(5);
+        lineDataSet.setDrawCircles(true);
+        lineDataSet.setDrawCircleHole(false);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(lineDataSet);
+
+        LineData data = new LineData(dataSets);
+        lineChart.setData(data);
+        lineChart.invalidate();
+    }
+
+    private ArrayList<Entry> dataValues () {
+        ArrayList<Entry> dataVals = new ArrayList<Entry>();
+
+        dataVals.add(new Entry(0, 20));
+        dataVals.add(new Entry(1, 24));
+        dataVals.add(new Entry(2, 2));
+        dataVals.add(new Entry(3, 10));
+        return dataVals;
+    }
+}
